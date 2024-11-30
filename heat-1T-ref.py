a2d64cab8ee2fd2e801ae7e9e91028edcbf8acab
linye-boli <linye@changchunboli.com>
2024-11-30 09:01:21 +0800

    modify heat-1T-ref.py

--- heat-1T-ref.py
+++ heat-1T-ref.py
@@ -6,8 +6,8 @@ if __name__ == "__main__":
     bcond = 'linear'
     gcond = 'const'
 
-    for dt in ['5.0e-3', '1.0e-3', '5.0e-4']:
-        for tol in ['1.0e-3', '1.0e-5', '1.0e-9']:
+    for dt in ['1.0e-3', '5.0e-4']:
+        for tol in ['1.0e-3', '1.0e-9']:
             
             outpath = f"./ref-solutions/heat-1T/z{zcond}-b{bcond}-g{gcond}/T1_{dt}-{tol}.txt"
             if os.path.exists(outpath):
